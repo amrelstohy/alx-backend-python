367a3b8ef0f7f6555ced70112ee2a0015680790c
Amr Elstohy <amramr2901@gmail.com>
2024-10-11 01:16:54 +0300

    hi

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Duck typing - first element of a sequence"""
+from typing import Union, Any, Sequence
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Duck typing - first element of a sequence"""
+    if lst:
+        return lst[0]
+    else:
+        return None
