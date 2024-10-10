2c3e66c9c21272bcecdfad192cab93442fcc833b
Amr Elstohy <amramr2901@gmail.com>
2024-10-10 19:01:09 +0000

    hi

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 """Basic annotations - concat"""
 
 
-def cocat(str1: str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """a type-annotated function concat"""
     return f"{str1}{str2}"
